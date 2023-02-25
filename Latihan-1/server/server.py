8f60fa09c2086eafbdf4ca513428032e981a4b5d
Okyan Awang Ramadhana <okyanawang@gmail.com>
2023-02-25 23:49:36 +0700

    Sub-point 1 Done

---
+++ Latihan-1/server/server.py
@@ -0,0 +1,36 @@
+import socket
+import os
+
+HOST = socket.gethostname()  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+# SERVER_DIR = '/path/to/server/folder'  # Change this to the path of your server folder
+SERVER_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def send_file(conn, filename):
+    try:
+        with open(os.path.join(SERVER_DIR, 'files', filename), 'rb') as f:
+            file_data = f.read()
+        conn.sendall(filename.encode() + b'\n' + file_data)
+        print('Sent', filename)
+    except FileNotFoundError:
+        conn.sendall(b'File not found')
+        print('File not found:', filename)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print('Server listening on', HOST, PORT)
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024).decode().strip()
+        if not data:
+            break
+        if data.startswith('download '):
+            filename = data.split()[1]
+            send_file(conn, filename)
+            break
+        else:
+            conn.sendall(b'Invalid command')
+            print('Invalid command:', data)
+    conn.close()
